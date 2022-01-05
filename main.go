595ac292bc52bb1dbfd889058d81610c7196a6ef
Donglin Liu <liudonglin301@gmail.com>
2022-01-04 22:04:45 -0500

    m

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func personCreate(w http.ResponseWriter, r *http.Request) {
 
     // Do something with the Person struct...
     fmt.Println("succ" )
-    fmt.Fprintf(w, "User: %+v", u)
+    fmt.Fprintf(w, "%+v", u)
 
 }
 
