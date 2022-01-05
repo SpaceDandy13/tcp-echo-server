0a641b8492aa3bda859cb779f8728b2bdc521afe
Donglin Liu <liudonglin301@gmail.com>
2022-01-04 22:00:50 -0500

    m

--- main.go
+++ main.go
@@ -20,8 +20,8 @@ func personCreate(w http.ResponseWriter, r *http.Request) {
     // respond to the client with the error message and a 400 status code.
     err := json.NewDecoder(r.Body).Decode(&u)
     if err != nil {
+    	fmt.Println("fail" )
         http.Error(w, err.Error(), http.StatusBadRequest)
-        fmt.Fprintf(w, "bad request%+v", u )
         return
     }
 
