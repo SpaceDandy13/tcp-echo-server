70bfafbdbda24eff8f157258b4167b0189520859
Donglin Liu <liudonglin301@gmail.com>
2022-01-04 21:20:35 -0500

    m

--- main.go
+++ main.go
@@ -1,62 +1,42 @@
-// Copyright 2018 Venil Noronha. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-	"net"
-	"os"
+    "errors"
+    "fmt"
+    "log"
+    "net/http"
 )
 
-// main serves as the program entry point
-func main() {
-	// obtain the port and prefix via program arguments
-	port := fmt.Sprintf(":%s", os.Args[1])
-	prefix := os.Args[2]
-
-	// create a tcp listener on the given port
-	listener, err := net.Listen("tcp", port)
-	if err != nil {
-		fmt.Println("failed to create listener, err:", err)
-		os.Exit(1)
-	}
-	fmt.Printf("listening on %s, prefix: %s\n", listener.Addr(), prefix)
-
-	// listen for new connections
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Println("failed to accept connection, err:", err)
-			continue
-		}
-
-		// pass an accepted connection to a handler goroutine
-		go handleConnection(conn, prefix)
-	}
+type User struct {
+    email string
+    password  string
 }
 
-// handleConnection handles the lifetime of a connection
-func handleConnection(conn net.Conn, prefix string) {
-	defer conn.Close()
-	reader := bufio.NewReader(conn)
-	for {
-		// read client request data
-		bytes, err := reader.ReadBytes(byte('\n'))
-		if err != nil {
-			if err != io.EOF {
-				fmt.Println("failed to read data, err:", err)
-			}
-			return
-		}
-		fmt.Printf("request: %s", bytes)
+func personCreate(w http.ResponseWriter, r *http.Request) {
+    var u User
 
-		// prepend prefix and send as response
-		line := fmt.Sprintf("%s %s", prefix, bytes)
-		fmt.Printf("response: %s", line)
-		conn.Write([]byte(line))
-	}
+    err := decodeJSONBody(w, r, &u)
+    if err != nil {
+        var mr *malformedRequest
+        if errors.As(err, &mr) {
+            http.Error(w, mr.msg, mr.status)
+        } else {
+            log.Println(err.Error())
+            http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+        }
+        return
+    }
+
+    fmt.Fprintf(w, "User: %+v", u)
+
+    
+}
+
+func main() {
+    mux := http.NewServeMux()
+    mux.HandleFunc("/person/create", personCreate)
+
+    log.Println("Starting server on :4000...")
+    err := http.ListenAndServe(":4000", mux)
+    log.Fatal(err)
 }
