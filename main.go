9c4f4af7ede600d31350583d178e6cb654ec75cb
Donglin Liu <liudonglin301@gmail.com>
2022-01-04 22:20:16 -0500

    m

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func personCreate(w http.ResponseWriter, r *http.Request) {
     fmt.Println("succ" )
     w.Header().Add("Content-Type", "application/json")
     w.WriteHeader(http.StatusCreated)
-    data, err = json.Marshal(u)
+    data, err := json.Marshal(u)
     w.Write(data)
 
 }
