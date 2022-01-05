347fe5c698449c59aa30ae21d231ae0a6d21c707
Donglin Liu <liudonglin301@gmail.com>
2022-01-04 21:36:28 -0500

    m

--- main.go
+++ main.go
@@ -7,25 +7,25 @@ import (
     "net/http"
 )
 
-type Person struct {
-    Name string
-    Age  int
+type User struct {
+    Email string
+    Password  int
 }
 
 func personCreate(w http.ResponseWriter, r *http.Request) {
     // Declare a new Person struct.
-    var p Person
+    var u User
 
     // Try to decode the request body into the struct. If there is an error,
     // respond to the client with the error message and a 400 status code.
-    err := json.NewDecoder(r.Body).Decode(&p)
+    err := json.NewDecoder(r.Body).Decode(&u)
     if err != nil {
         http.Error(w, err.Error(), http.StatusBadRequest)
         return
     }
 
     // Do something with the Person struct...
-    fmt.Fprintf(w, "Person: %+v", p)
+    fmt.Fprintf(w, "User: %+v", u)
 }
 
 func main() {
